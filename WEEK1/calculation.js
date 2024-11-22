5f2dd5de0c3b4dae1d9891dcec2873052673db81
rainvenkat <ranivenkat1@gmail.com>
2024-11-22 20:26:11 +0000

    Add files via upload

---
+++ WEEK1/calculation.js
@@ -0,0 +1,35 @@
+//LAB EXERCISE
+
+//programm that functions addition subtraction multiplication and division
+
+//Addition
+const num1 = 650;
+const num2 = 200;
+// add two numbers
+const sum = num1 + num2;
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+//Subtraction
+const num3 = 30;
+const num4 = 15;
+// subtract two numbers
+const sub = num3 - num4;
+// display the difference
+console.log('The difference between ' + num3 + ' and ' + num4 + ' is: ' + sub);
+
+//multiplication
+const num5 = 443;
+const num6 = 25;
+// multiply two numbers
+const mul = num5 * num6;
+// display the multiplication
+console.log('The multiplication of ' + num5 + ' and ' + num6 + ' is: ' + mul);
+
+//division
+const num7 = 1000;
+const num8 = 500;
+// divide two numbers
+const div = num7 / num8;
+// display the division
+console.log('The division of ' + num7 + ' and ' + num8 + ' is: ' + div);
---
+++ WEEK1/index.js
@@ -0,0 +1,31 @@
+//Exercise 1
+console.log("This is my first practical class")
+console.log("My first program")
+
+//Example 1
+const num1 = 10;
+const num2 = 20;
+// add two numbers
+const sum = num1 + num2;
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+//Example 2
+// program that checks if the number is positive, negative or zero
+// input from the user
+
+var prompt = require("prompt-sync")();
+const number = parseInt(("Enter a number: "));
+
+check if number is greater than 0
+if (number > 0) {
+ console.log("The number is positive");
+}
+// check if number is 0
+else if (number == 0) {
+ console.log("The number is zero");
+}
+// if number is less than 0
+else {
+ console.log("The number is negative");
+}
